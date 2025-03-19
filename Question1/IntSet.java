050a6683eae1de661d886508a2b85d93b7c468e1
Shreyansh Agrawal <shreyansh.agrawal@metacube.com>
2025-03-19 16:20:34 +0530

    added: intset modifications with test cases

--- Question1/IntSet.java
+++ Question1/IntSet.java
@@ -2,13 +2,14 @@ package Question1;
 
 final class IntSet {
     private final boolean arr[]; 
-
+    int maxSize; 
     /**
      * @param arr input boolean array to set if the number is in set or not
      * @throws IllegalArgumentException when input array is not of size 1000
      */
     IntSet(boolean arr[])throws IllegalArgumentException{
-        if(arr.length!=1000)throw new IllegalArgumentException("Enter array of valid size");
+        if(arr.length>1000)throw new IllegalArgumentException("Enter array of valid size");
+        this.maxSize = arr.length; 
         this.arr = arr;  
     }
 
@@ -18,8 +19,8 @@ final class IntSet {
      * @throws IllegalArgumentException when x is below 1 or above 1000
      */
     public boolean isMember(int x)throws IllegalArgumentException{
-        if(x<0||x>999)throw new IllegalArgumentException("Set can have number between 1 to 1000");
-        return this.arr[x-1]; 
+        if(x<0||x>maxSize)throw new IllegalArgumentException("Set can have number between 1 to "+maxSize);
+        return this.arr[x+1];
     }
 
     /**
@@ -27,7 +28,7 @@ final class IntSet {
      */
     public int size(){
         int size = 0; 
-        for(int i = 0; i<1000;i++)if(this.arr[i])size++; 
+        for(int i = 0; i<maxSize;i++)if(this.arr[i])size++; 
         return size; 
     }
 
@@ -36,7 +37,8 @@ final class IntSet {
      * @return if s is subset or not
      */
     public boolean isSubSet(IntSet s){
-        for(int i = 0;i<1000;i++)if(s.arr[i]&&!this.arr[i])return false; 
+        if(s.maxSize>maxSize)throw new IllegalArgumentException("Subset can't be greater than set"); 
+        for(int i = 0;i<maxSize;i++)if(s.arr[i]&&!this.arr[i])return false; 
         return true; 
     }
 
@@ -45,7 +47,7 @@ final class IntSet {
      */
     public IntSet getComplementSet(){
         boolean []newArr = new boolean[1000]; 
-        for(int i = 0;i<1000;i++)newArr[i] = !(this.arr[i]); 
+        for(int i = 0;i<maxSize;i++)newArr[i] = !(this.arr[i]); 
         return new IntSet(newArr);  
     }
     
@@ -55,7 +57,12 @@ final class IntSet {
      */
     public IntSet union(IntSet s){
         boolean []newArr = new boolean[1000]; 
-        for(int i = 0;i<1000;i++)if(this.arr[i]||s.arr[i])newArr[i]=true; 
+        for(int i = 0;i<Math.min(maxSize,s.maxSize);i++)if(this.arr[i]||s.arr[i])newArr[i]=true; 
+        if(maxSize>s.maxSize){
+            for(int i = s.maxSize; i<maxSize;i++)if(this.arr[i])newArr[i] = true; 
+        }else{
+            for(int i = maxSize;i<s.maxSize;i++)if(s.arr[i])newArr[i]=true; 
+        }
         return new IntSet(newArr);
     }
 }
--- Question1/TestQues.java
+++ Question1/TestQues.java
@@ -1,27 +1,86 @@
 package Question1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 
-public class TestQues {
-    static IntSet set; 
-
-    @BeforeAll static void setup(){
-        boolean temp[] = new boolean[1000]; 
-        temp[2] = true;     
-        temp[7] = true;     
-        temp[34] = true;     
-        temp[28] = true;     
-        temp[8] = true;     
-        temp[11] = true;     
-        temp[9] = true;  
-        set = new IntSet(temp);    
+class IntSetTest {
+    @Test
+    void testConstructor() {
+        boolean[] arr = new boolean[1000];
+        assertDoesNotThrow(() -> new IntSet(arr));
+        assertThrows(IllegalArgumentException.class, () -> new IntSet(arr));
     }
 
     @Test
-    public void testSize(){
-        assertEquals(7, set.size());
+    void testIsMember() {
+        boolean[] arr = new boolean[1000];
+        arr[5] = true;
+        IntSet set = new IntSet(arr);
+
+        assertTrue(set.isMember(5));
+    }
+
+    @Test
+    void testSize() {
+        boolean[] arr = new boolean[1000];
+        arr[2] = true;
+        arr[5] = true;
+        arr[999] = true;
+        IntSet set = new IntSet(arr);
+
+        assertEquals(3, set.size());
+    }
+
+    @Test
+    void testIsSubSet() {
+        boolean[] arr1 = new boolean[1000];
+        arr1[2] = true;
+        arr1[5] = true;
+        arr1[10] = true;
+        IntSet set1 = new IntSet(arr1);
+
+        boolean[] arr2 = new boolean[1000];
+        arr2[5] = true;
+        IntSet set2 = new IntSet(arr2);
+
+        assertTrue(set1.isSubSet(set2));
+    }
+
+    @Test
+    void testGetComplementSet() {
+        boolean[] arr = new boolean[1000];
+        arr[2] = true;
+        arr[5] = true;
+        IntSet set = new IntSet(arr);
+
+        IntSet complementSet = set.getComplementSet();
+        assertFalse(complementSet.isMember(2));
+        assertFalse(complementSet.isMember(5));
+        assertTrue(complementSet.isMember(3));
+    }
+
+    @Test
+    void testUnion() {
+        boolean[] arr1 = new boolean[1000];
+        arr1[1] = true;
+        arr1[5] = true;
+        IntSet set1 = new IntSet(arr1);
+
+        boolean[] arr2 = new boolean[1000];
+        arr2[2] = true;
+        arr2[5] = true;
+        IntSet set2 = new IntSet(arr2);
+
+        IntSet unionSet = set1.union(set2);
+
+        assertTrue(unionSet.isMember(1));
+        assertTrue(unionSet.isMember(2));
+        assertTrue(unionSet.isMember(5));
+        assertFalse(unionSet.isMember(3));
     }
 }
